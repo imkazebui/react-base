74d30633671fa74fb04b53c343a7f995442edee1
PhuongBui <phuong.bui@saigontechnology.com>
2020-07-18 12:42:33 +0700

    add loading

--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import NProgress from 'nprogress';
 import API_DOMAIN from 'constants/api';
 import { LANGUAGE, TOKEN } from 'constants/cookies';
-
 import { isLocalHost } from 'utilities/url';
+
+import 'nprogress/nprogress.css';
+
+let requestsCounter = 0;
 
 const instance = axios.create({
   baseURL: `${API_DOMAIN}/`,
@@ -11,6 +15,9 @@ const instance = axios.create({
 
 // Set the AUTH token for any request
 instance.interceptors.request.use(function (config) {
+  requestsCounter++;
+  NProgress.start();
+
   let token = Cookies.get(TOKEN);
 
   if (isLocalHost) {
@@ -28,6 +35,11 @@ instance.interceptors.response.use(
   function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
+
+    if (--requestsCounter === 0) {
+      NProgress.done();
+    }
+
     return response;
   },
   function (error) {
@@ -38,6 +50,10 @@ instance.interceptors.response.use(
     // Object.entries(error).map(([label, value], idx) => {
     //   console.log(`idx ${idx}`, label, value);
     // });
+
+    if (--requestsCounter === 0) {
+      NProgress.done();
+    }
 
     const { status } = error.response;
 
@@ -58,5 +74,12 @@ instance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+const updateProgress = (e) =>
+  NProgress.inc(calculatePercentage(e.loaded, e.total));
+const calculatePercentage = (loaded, total) => Math.floor(loaded * 1.0) / total;
+
+instance.defaults.onDownloadProgress = updateProgress;
+instance.defaults.onUploadProgress = updateProgress;
 
 export default instance;
