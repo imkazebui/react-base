7d2e6dbd384ba899a4d18e5e63ff926508a571ca
Phuong <phuong@STS-PHUONGBUI.local>
2021-01-26 15:18:45 +0700

    add react query

--- src/App.js
+++ src/App.js
@@ -4,27 +4,32 @@ import { IntlProvider } from 'react-intl';
 import { PrivateRoute, LoadingScreen } from 'components/Atoms';
 import { PrivateLayout } from 'components/Templates';
 import renderRoutes from 'routers/render';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 import messages from 'translations';
+
+const queryClient = new QueryClient();
 
 function App() {
   const language = 'en';
 
   return (
     <React.StrictMode>
-      <IntlProvider locale={language} messages={messages[language]}>
-        <Router>
-          <Suspense fallback={<LoadingScreen />}>
-            <Switch>
-              {renderRoutes()}
+      <QueryClientProvider client={queryClient}>
+        <IntlProvider locale={language} messages={messages[language]}>
+          <Router>
+            <Suspense fallback={<LoadingScreen />}>
+              <Switch>
+                {renderRoutes()}
 
-              <Route path="/">
-                <PrivateLayout />
-              </Route>
-            </Switch>
-          </Suspense>
-        </Router>
-      </IntlProvider>
+                <Route path="/">
+                  <PrivateLayout />
+                </Route>
+              </Switch>
+            </Suspense>
+          </Router>
+        </IntlProvider>
+      </QueryClientProvider>
     </React.StrictMode>
   );
 }
