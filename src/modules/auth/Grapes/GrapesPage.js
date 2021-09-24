2580f785d91b21150db7f404257a833f748184c1
phuongbui <phuongbui@phuongbuis-MacBook-Pro.local>
2021-09-25 00:26:20 +0700

    update code grapejs

---
+++ src/modules/auth/Grapes/GrapesPage.js
@@ -0,0 +1,128 @@
+import React, { useEffect } from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import { Form, Col } from 'antd';
+
+import grapesjs from 'grapesjs';
+import gjsBasicBlocks from 'grapesjs-blocks-basic';
+
+import 'grapesjs/dist/css/grapes.min.css';
+import './styles.scss';
+
+const CustomImage = (editor) => {
+  editor.DomComponents.addType('custom-img', {
+    isComponent: (el) => el.tagName === 'a',
+    extends: 'image',
+    model: {
+      // Default properties
+      defaults: {
+        tagName: 'a',
+        attributes: {
+          // Default attributes
+          href: 'http://soaica.dev/',
+        },
+        traits: ['href'],
+        components: [
+          {
+            tagName: 'img',
+            attributes: {
+              src: 'https://i.pinimg.com/originals/c6/9f/e1/c69fe13923071f4d491be72790d11ff8.jpg',
+              width: 50,
+              height: 50,
+            },
+            traits: ['src'],
+            content: '',
+          },
+        ],
+      },
+    },
+    // view: {
+    //   onRender() {
+    //     console.log('Local hook: view.onRender');
+    //     editor.runCommand('command-modal-image');
+    //   },
+    // },
+  });
+};
+
+const GrapesPage = () => {
+  useEffect(() => {
+    const editor = grapesjs.init({
+      fromElement: 1,
+      container: '#gjs',
+      plugins: ['gjs-blocks-basic', CustomImage],
+    });
+
+    const blockManager = editor.BlockManager;
+    blockManager.add('my-first-block', {
+      label: 'Custom image',
+      content: {
+        type: 'custom-img',
+      },
+      // content: {
+      //   tagName: 'a',
+      //   attributes: { href: 'href-default' },
+      //   components: [
+      //     {
+      //       tagName: 'img',
+      //       attributes: { src: 'tagname' },
+      //       content: 'h1 content',
+      //     },
+      //   ],
+      // },
+      activate: true,
+    });
+
+    const commands = editor.Commands;
+
+    const modal = (
+      <div>
+        <input id="custom-img-href"></input>
+        <input id="custom-img-src"></input>
+      </div>
+    );
+
+    commands.add('command-modal-image', {
+      run(editor) {
+        editor.Modal.open({
+          title: 'Modal example',
+          content: ReactDOMServer.renderToString(modal),
+        }).onceClose(() => this.stopCommand());
+
+        document
+          .getElementById('custom-img-href')
+          .addEventListener('change', ({ target }) => {
+            const component = editor.getSelected();
+            component.updateTrait('href', {
+              value: target.value,
+            });
+          });
+
+        document
+          .getElementById('custom-img-src')
+          .addEventListener('change', ({ target }) => {
+            const component = editor.getSelected();
+            const allImages = component.find('img')[0];
+            allImages.updateTrait('src', {
+              value: target.value,
+            });
+          });
+      },
+    });
+
+    editor.on('block:drag:stop', (some, argument) => {
+      if (some && argument.attributes.label === 'Custom image') {
+        editor.runCommand('command-modal-image');
+        const component = editor.getSelected();
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <div id="gjs"></div>
+    </>
+  );
+};
+
+export default GrapesPage;
--- src/modules/auth/routes.js
+++ src/modules/auth/routes.js
@@ -1,8 +1,10 @@
 import { lazy } from 'react';
 const Login = lazy(() => import('./Login'));
+const Grapes = lazy(() => import('./Grapes'))
 
 export const routePath = {
   login: '/login',
+  grapes: '/grapes',
 };
 
 export const routes = [
@@ -11,6 +13,14 @@ export const routes = [
     component: Login,
     exact: true,
     name: 'Login',
+    showMenu: false,
+    icon: <span>hello</span>,
+  },
+  {
+    path: routePath.grapes,
+    component: Grapes,
+    exact: true,
+    name: 'grapes',
     showMenu: false,
     icon: <span>hello</span>,
   },
