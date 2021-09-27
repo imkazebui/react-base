adfc226fcfe31e4a25889b27242f1a1a521b814c
phuongbui <phuongbui@phuongbuis-MacBook-Pro.local>
2021-09-27 16:41:24 +0700

    re-factor code

---
+++ src/modules/auth/Grapes/CustomImageBlock.js
@@ -0,0 +1,105 @@
+import React, { useEffect } from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { Col, Input, Row } from 'antd';
+
+const CustomImageBlock = (editor) => {
+  editor.DomComponents.addType('custom-img', {
+    isComponent: (el) => el.tagName === 'a',
+    extends: 'image',
+    model: {
+      // Default properties
+      defaults: {
+        tagName: 'a',
+        attributes: {
+          // Default attributes
+          href: 'http://soaica.dev/',
+        },
+        traits: ['href'],
+        components: [
+          {
+            tagName: 'img',
+            attributes: {
+              src: 'https://i.pinimg.com/originals/c6/9f/e1/c69fe13923071f4d491be72790d11ff8.jpg',
+              width: 50,
+              height: 50,
+            },
+            traits: ['src'],
+            content: '',
+          },
+        ],
+      },
+    },
+  });
+
+  const blockManager = editor.BlockManager;
+  blockManager.add('custom-image-block', {
+    label: 'Custom image',
+    content: {
+      type: 'custom-img',
+    },
+    activate: true,
+  });
+
+  const commands = editor.Commands;
+
+  const modal = (
+    <>
+      <Row gutter="16">
+        <Col span="12">
+          <label>Href</label>
+          <Input id="custom-img-href" />
+        </Col>
+        <Col span="12">
+          <label>Source Tag</label>
+          {/* <Input id="custom-img-src" /> */}
+          <select name="cars" id="custom-img-src">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+          </select>
+        </Col>
+      </Row>
+      <br />
+    </>
+  );
+
+  commands.add('command-modal-image', {
+    run(editor) {
+      editor.Modal.open({
+        title: 'Custom Image',
+        content: ReactDOMServer.renderToString(modal),
+      }).onceClose(() => this.stopCommand());
+
+      document
+        .getElementById('custom-img-href')
+        .addEventListener('change', ({ target }) => {
+          const component = editor.getSelected();
+          component.updateTrait('href', {
+            value: target.value,
+          });
+          component.setAttributes({ href: target.value });
+        });
+
+      document
+        .getElementById('custom-img-src')
+        .addEventListener('change', ({ target }) => {
+          const component = editor.getSelected();
+          const allImages = component.find('img')[0];
+          allImages.updateTrait('src', {
+            value: target.value,
+          });
+          allImages.setAttributes({ src: target.value });
+        });
+    },
+  });
+
+  editor.on('block:drag:stop', (some, argument) => {
+    if (some && argument.attributes.label === 'Custom image') {
+      editor.runCommand('command-modal-image');
+      const component = editor.getSelected();
+    }
+  });
+};
+
+export default CustomImageBlock;
